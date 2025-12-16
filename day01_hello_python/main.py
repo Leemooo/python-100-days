eda6d523d8b7d303026efc3e4a157e2c63a1c9b1
Leemooo <Liamtdixon1995@gmail.com>
2025-12-16 16:08:11 +0000

    Create main.py
    All Python code from day 1

---
+++ day01_hello_python/main.py
@@ -0,0 +1,10 @@
+print("Hello, Python!")
+print("This is Day 1 of my 100-day Python challenge.")
+
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}!")
+
+goal = input("What is your goal with python? ")
+print(f"Python will help you with {goal}!")
+
+print("Welcome to your Python journey.")
